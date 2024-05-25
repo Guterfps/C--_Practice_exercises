e9c4f212f95e2b9f5c08bf57d8c6882050503d8f
guterfps <ithayg@gmail.com>
2024-05-25 16:00:25 +0300

    sort stack recursivly

--- sort_stack.cpp
+++ sort_stack.cpp
@@ -2,34 +2,22 @@
 #include <stack>
 #include <iostream>
 #include <random>
+#include <functional>
 
 void IterativeSortStack(std::stack<int> *st);
 void RecSortStack(std::stack<int> *st);
+void RecSortInsert(std::stack<int> *st, int num);
 bool IsStackSorted(std::stack<int> st);
+void TestSortStack(std::function<void(std::stack<int> *st)> sort_func);
 
 
 int main(void)
 {
-    std::stack<int> st;
-    std::random_device dev;
-    std::mt19937 rng(dev());
-    std::uniform_int_distribution<std::mt19937::result_type> dist(-10,10);
+    std::cout << "Iterative sort:\n";
+    TestSortStack(&IterativeSortStack); 
 
-    st.push(dist(rng));
-    st.push(dist(rng));
-    st.push(dist(rng));
-    st.push(dist(rng));
-    st.push(dist(rng));
-    st.push(dist(rng));
-
-    std::cout << "is stack sorted before: " << 
-    (IsStackSorted(st) ? "yes" : "no") << '\n'; 
-
-    IterativeSortStack(&st);
-
-    std::cout << "is stack sorted after: " << 
-    (IsStackSorted(st) ? "yes✅" : "no❌") << '\n'; 
-
+    std::cout << "Recursive sort:\n";
+    TestSortStack(&RecSortStack);
 }
 
 void IterativeSortStack(std::stack<int> *st)
@@ -57,6 +45,34 @@ void IterativeSortStack(std::stack<int> *st)
     }
 }
 
+void RecSortStack(std::stack<int> *st)
+{
+    if (!st->empty())
+    {
+        int tmp = st->top();
+        st->pop();
+
+        RecSortStack(st);
+
+        RecSortInsert(st, tmp);
+    }
+}
+
+void RecSortInsert(std::stack<int> *st, int num)
+{
+    if (st->empty() || (num < st->top()))
+    {
+        st->push(num);
+    }
+    else
+    {
+        int tmp = st->top();
+        st->pop();
+        RecSortInsert(st, num);
+        st->push(tmp);
+    }
+}
+
 bool IsStackSorted(std::stack<int> st)
 {
     int prev = st.top();
@@ -70,4 +86,27 @@ bool IsStackSorted(std::stack<int> st)
     }
 
     return status;
+}
+
+void TestSortStack(std::function<void(std::stack<int> *st)> sort_func)
+{
+    std::stack<int> st;
+    std::random_device dev;
+    std::mt19937 rng(dev());
+    std::uniform_int_distribution<std::mt19937::result_type> dist(-10,10);
+
+    st.push(dist(rng));
+    st.push(dist(rng));
+    st.push(dist(rng));
+    st.push(dist(rng));
+    st.push(dist(rng));
+    st.push(dist(rng));
+
+    std::cout << "is stack sorted before: " << 
+    (IsStackSorted(st) ? "yes" : "no") << '\n'; 
+
+    sort_func(&st);
+
+    std::cout << "is stack sorted after: " << 
+    (IsStackSorted(st) ? "yes✅" : "no❌") << '\n'; 
 }
